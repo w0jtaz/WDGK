a09225e5f32d1888e717acbed8c475277193894d
w0jtaz <73688850+w0jtaz@users.noreply.github.com>
2022-10-12 19:20:52 +0200

    dodanie uzupełnionego lab1

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,13 +1,14 @@
 import numpy as np
 
+print()
 # zadanie1
 array = np.array([5]*50)
 print(array)
 
 print()
 # zadanie2
-tablica = np.random.randint(1, 25, (5, 5))
-print(tablica)
+tablica_zad2 = np.arange(1,26).reshape(5, 5)
+print(tablica_zad2)
 
 print()
 # zadanie3
@@ -20,6 +21,30 @@ zera = np.zeros((5, 5), int)
 np.fill_diagonal(zera, 8)
 print(zera)
 
+print()
 # zadanie5
-tablica = np.random.randint(1, 5[0.01], (5, 5))
-print(tablica)
+tablica = np.arange(0.01,1.01,0.01)
+print(tablica.reshape((10,10)))
+
+print()
+# zadanie6
+print(np.linspace(0,1,50))
+
+print()
+# zadanie7
+podtablica = tablica_zad2[2:5,1:5]
+print(podtablica)
+
+print()
+# zadanie8
+podtablica = tablica_zad2[:3,4].reshape(3,1)
+print(podtablica)
+
+print()
+# zadanie9
+suma= tablica_zad2[3]+tablica_zad2[4]
+print(suma.sum())
+
+print()
+# zadanie10
+#nie mam pomysłu jak to rozwiązać
