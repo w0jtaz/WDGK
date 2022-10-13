3e8b9487ec14eff6c3bc05f04248b1f2a7d84e9a
w0jtaz <73688850+w0jtaz@users.noreply.github.com>
2022-10-13 11:09:22 +0200

    tworzenie biblioteki

---
+++ lab2/lab2.py
@@ -0,0 +1,70 @@
+import  numpy as np
+from enum import Enum
+from matplotlib.image import imread
+
+
+
+class ColorModel(Enum):
+    rgb = 0
+    hsv = 1
+    hsi = 2
+    hsl = 3
+    gray = 4  # obraz 2d
+
+class BaseImage:
+    data: np.ndarray  # tensor przechowujacy piksele obrazu
+    color_model: ColorModel  # atrybut przechowujacy biezacy model barw obrazu
+
+    def __init__(self, path: str) -> None:
+        """
+        inicjalizator wczytujacy obraz do atrybutu data na podstawie sciezki
+        """
+        data = imread(path)
+
+        pass
+
+    def save_img(self, path: str) -> None:
+        """
+        metoda zapisujaca obraz znajdujacy sie w atrybucie data do pliku
+        """
+        pass
+
+    def show_img(self) -> None:
+        """
+        metoda wyswietlajaca obraz znajdujacy sie w atrybucie data
+        """
+        pass
+
+    def get_layer(self, layer_id: int) -> 'Image':
+        """
+        metoda zwracajaca warstwe o wskazanym indeksie
+        """
+        pass
+
+    def to_hsv(self) -> 'Image':
+        """
+        metoda dokonujaca konwersji obrazu w atrybucie data do modelu hsv
+        metoda zwraca nowy obiekt klasy image zawierajacy obraz w docelowym modelu barw
+        """
+        pass
+
+    def to_hsi(self) -> 'Image':
+        """
+        metoda dokonujaca konwersji obrazu w atrybucie data do modelu hsi
+        metoda zwraca nowy obiekt klasy image zawierajacy obraz w docelowym modelu barw
+        """
+        pass
+
+    def to_hsl(self) -> 'Image':
+        """
+        metoda dokonujaca konwersji obrazu w atrybucie data do modelu hsl
+        metoda zwraca nowy obiekt klasy image zawierajacy obraz w docelowym modelu barw
+        """
+        pass
+
+    def to_rgb(self) -> 'Image':
+        """
+        metoda dokonujaca konwersji obrazu w atrybucie data do modelu rgb
+        metoda zwraca nowy obiekt klasy image zawierajacy obraz w docelowym modelu barw
+        """
+        pass
