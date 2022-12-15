9cfe3feb098b93bfa55cd9ced5219684d0a1e652
w0jtaz <73688850+w0jtaz@users.noreply.github.com>
2022-12-15 10:46:22 +0100

    update

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -1,6 +1,6 @@
 from lab2.lab2 import BaseImage
 from lab3.lab3 import GrayScaleTransform
-from lab4.lab4 import ImageComparison
+# from lab4.lab4 import ImageComparison
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -48,30 +48,67 @@ class ImageAligning(BaseImage):
         """
         super().__init__(path)
 
-    def align_image(self, tail_elimination: bool = True) -> 'BaseImage':
+    def align_channel(self, channel: np.ndarray, tail_elimination: bool = False) -> np.ndarray:
+        """
+        metoda wyrównująca histogram danego kanału
+        """
+        channel = channel.astype(np.float64)
+        rows, cols = self.data.shape[:2]
+        max_value = np.max(channel)
+        min_value = np.min(channel)
+        if tail_elimination == True:
+            max_value = np.percentile(channel, 95)
+            min_value = np.percentile(channel, 5)
+        for i in range(rows):
+            for j in range(cols):
+                try:
+                    channel[i, j] = ((channel[i, j] - min_value) / (max_value - min_value)) * 255
+                except ZeroDivisionError:
+                    print("ZeroDivisionError")
+        channel[channel > 255] = 255
+        channel[channel < 0] = 0
+        return channel.astype('uint8')
+
+    def align_image(self, tail_elimination: bool = False) -> 'BaseImage':
+        """
+        metoda wyrównująca histogram obrazu
+        """
+        if self.data.shape[-1] == 3:
+            r = self.align_channel(self.data[:, :, 0], tail_elimination)
+            g = self.align_channel(self.data[:, :, 1], tail_elimination)
+            b = self.align_channel(self.data[:, :, 2], tail_elimination)
+            self.data = np.dstack((r, g, b))
+        else:
+            self.data = self.align_channel(self.data, tail_elimination)
+        return self
 
 
 
 
-class Image(GrayScaleTransform, ImageComparison, ImageAligning):
+class Image(GrayScaleTransform, ImageAligning):
     def __init__(self, path: str):
         super().__init__(path)
 
     pass
 
 
+#
+# x = Image("lena.jpg")
+# x.to_gray()
+# histogram = Histogram(x.data)
+# histogram.to_cumulated().plot()
+#
+#
+# y = Image("lena.jpg")
+# y= Histogram(y.data)
+# y.to_cumulated().plot()
+#
+# z= Image("lena.jpg")
+# z.to_gray()
+# z = Histogram(z.data)
+# z.plot()
 
-x = Image("lena.jpg")
-x.to_gray()
-histogram = Histogram(x.data)
-histogram.to_cumulated().plot()
-
-
-y = Image("lena.jpg")
-y= Histogram(y.data)
-y.to_cumulated().plot()
-
-z= Image("lena.jpg")
-z.to_gray()
-z = Histogram(z.data)
-z.plot()
+q = Image("lena.jpg")
+q.align_image(True)
+q = Histogram(q.data)
+q.plot()
